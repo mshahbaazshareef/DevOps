7b2a9d2fd71109b1e9418e4e92eed4f97b3c3d1f
mshahbaazshareef <mshahbaazshareef@gmail.com>
2020-08-16 16:36:01 +0530

    updating shebang

--- Shell_Scripts/shebang.sh
+++ Shell_Scripts/shebang.sh
@@ -1,3 +1,10 @@
+
+
+
 # this line is to demonstrate the shebang  line
 # this line is used to set absolute path to the interpreter for executing  this script ex "#!bin/bashsrc"
+#commited to github with seperate directories.
+
+
+
 
