84eaae6c58891958f190eabde9e71561b601badf
mshahbaazshareef <mshahbaazshareef@gmail.com>
2020-08-19 22:50:43 +0530

    modified shebang and created branch1.txt

--- Shell_Scripts/shebang.sh
+++ Shell_Scripts/shebang.sh
@@ -4,6 +4,7 @@
 # this line is to demonstrate the shebang  line
 # this line is used to set absolute path to the interpreter for executing  this script ex "#!bin/bashsrc"
 #commited to github with seperate directories.
+# testing a conflict , modifying the shebang file from branch 1
 
 
 
